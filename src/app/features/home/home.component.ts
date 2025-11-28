82eb723552c305971adbc80ce67f3ad95f7f9585
Juan Costa <juancostasil16@gmail.com>
2025-11-27 23:23:32 -0300

    Att

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Opportunity, OpportunityService } from '../../core/services/opportunity.service';
+import { OrganizationService } from '../../core/services/organization.service';
 
 @Component({
     selector: 'app-home',
@@ -9,17 +10,49 @@ import { Opportunity, OpportunityService } from '../../core/services/opportunity
 })
 export class HomeComponent implements OnInit {
     private opportunityService = inject(OpportunityService);
+    private organizationService = inject(OrganizationService);
 
     opportunities = signal<Opportunity[]>([]);
     currentSlide = 0;
 
+    // Dynamic statistics
+    stats = signal({
+        volunteers: 0,
+        organizations: 0,
+        livesImpacted: 0,
+        cities: 0
+    });
+
     ngOnInit() {
         this.loadOpportunities();
+        this.loadStatistics();
     }
 
     loadOpportunities() {
         this.opportunityService.getOpportunities().subscribe(data => {
             this.opportunities.set(data);
+        });
+    }
+
+    loadStatistics() {
+        this.organizationService.getOrganizations().subscribe(orgs => {
+            // Calculate total volunteers from all organizations
+            const totalVolunteers = orgs.reduce((sum, org) => sum + (org.volunteers || 0), 0);
+
+            // Calculate total lives impacted (volunteers * 2 as approximation)
+            const livesImpacted = totalVolunteers * 2;
+
+            // Get unique cities from opportunities
+            this.opportunityService.getOpportunities().subscribe(opps => {
+                const uniqueCities = new Set(opps.map(opp => opp.location.split(',')[0].trim()));
+
+                this.stats.set({
+                    volunteers: totalVolunteers,
+                    organizations: orgs.length,
+                    livesImpacted: livesImpacted,
+                    cities: uniqueCities.size
+                });
+            });
         });
     }
 
